cb3e262a9986c87e460f7f16cb89943f084398fe
Robson Frias <robsonfrias@gmail.com>
2019-09-16 22:08:21 -0300

    MyPhotos/visualizar

--- app/src/main/java/br/com/etechoracio/myphoto/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/myphoto/MainActivity.java
@@ -52,7 +52,14 @@ public class MainActivity extends AppCompatActivity {
         {
             super.onActivityResult(requestCode, resultCode, data);
         }
-    }
 
+
+        }
+    public void onVisualizar(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, "image/jpeg");
+        startActivity(intent);
+    }
 }
 
